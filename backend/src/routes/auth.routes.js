820a66bf0eb963f0eff174209b7cee038dd02f93
Thnh8052 <vuthanh250804@gmail.com>
2025-11-18 22:32:50 +0700

    update forgot password

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { register, login, getMe } from "../controllers/auth.controller.js";
+import { register, login, getMe,resetPassword,forgotPassword } from "../controllers/auth.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.put("/reset-password/:token", resetPassword);
 router.get("/me", protect, getMe);
 
 export default router;
